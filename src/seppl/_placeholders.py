19e33b5f0077ae7a8b513dba1ea79cd99332c686
Peter Reutemann <fracpete@gmail.com>
2025-03-12 17:00:38 +1300

    added HOME/CWD/TMP placeholders

--- src/seppl/_placeholders.py
+++ src/seppl/_placeholders.py
@@ -1,5 +1,9 @@
 import os
+import tempfile
 
+PH_HOME = "{HOME}"
+PH_CWD = "{CWD}"
+PH_TMP = "{TMP}"
 PH_INPUT_PATH = "{INPUT_PATH}"
 PH_INPUT_NAMEEXT = "{INPUT_NAMEEXT}"
 PH_INPUT_NAMENOEXT = "{INPUT_NAMENOEXT}"
@@ -7,6 +11,9 @@ PH_INPUT_EXT = "{INPUT_EXT}"
 PH_INPUT_PARENT_PATH = "{INPUT_PARENT_PATH}"
 PH_INPUT_PARENT_NAME = "{INPUT_PARENT_NAME}"
 PLACEHOLDERS = [
+    PH_HOME,
+    PH_CWD,
+    PH_TMP,
     PH_INPUT_PATH,
     PH_INPUT_NAMEEXT,
     PH_INPUT_NAMENOEXT,
@@ -15,6 +22,9 @@ PLACEHOLDERS = [
     PH_INPUT_PARENT_NAME,
 ]
 PLACEHOLDERS_HELP = {
+    PH_HOME: "The home directory of the current user.",
+    PH_CWD: "The current working directory.",
+    PH_TMP: "The temp directory.",
     PH_INPUT_PATH: "The directory part of the current input, i.e., '/some/where' of input '/some/where/file.txt'.",
     PH_INPUT_NAMEEXT: "The name (incl extension) of the current input, i.e., 'file.txt' of input '/some/where/file.txt'.",
     PH_INPUT_NAMENOEXT: "The name (excl extension) of the current input, i.e., 'file' of input '/some/where/file.txt'.",
@@ -58,7 +68,13 @@ def expand_placeholders(current_input: str, template: str) -> str:
                 value = None
                 if dir_name is None:
                     dir_name = ""
-                if ph == PH_INPUT_PATH:
+                if ph == PH_HOME:
+                    value = os.path.expanduser("~")
+                elif ph == PH_CWD:
+                    value = os.getcwd()
+                elif ph == PH_TMP:
+                    value = tempfile.gettempdir()
+                elif ph == PH_INPUT_PATH:
                     value = dir_name
                 elif ph == PH_INPUT_NAMEEXT:
                     value = os.path.basename(current_input)
